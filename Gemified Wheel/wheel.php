2e39bf11181544c757a2f0b764c3461212cf389a
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2025-01-31 19:39:09 +0600

    Update wheel.php

--- Gemified Wheel/wheel.php
+++ Gemified Wheel/wheel.php
@@ -8,8 +8,21 @@
 </head>
 <body>
     <div class="game-container">
-        <div class="wheel" id="wheel"></div>
-        <button id="spinButton">Spin for Prize</button>
+        <div class="wheel-container">
+            <div class="selector"></div>
+            <div class="wheel" id="wheel">
+                <div class="prize" style="transform: rotate(22.5deg);"><span>$10 Gift Card</span></div>
+                <div class="prize" style="transform: rotate(67.5deg);"><span>Free Shopping</span></div>
+                <div class="prize" style="transform: rotate(112.5deg);"><span>$5 Voucher</span></div>
+                <div class="prize" style="transform: rotate(157.5deg);"><span>Free Shipping</span></div>
+                <div class="prize" style="transform: rotate(202.5deg);"><span>$10 Cash</span></div>
+                <div class="prize" style="transform: rotate(247.5deg);"><span>Free Chocolate</span></div>
+                <div class="prize" style="transform: rotate(292.5deg);"><span>$20 Spree</span></div>
+                <div class="prize" style="transform: rotate(337.5deg);"><span>Free Membership</span></div>
+            </div>
+            <button id="spinButton">Spin for Prize</button>
+        </div>
+        
     </div>
     <div class="modal" id="prizeModal">
         <div class="modal-content">
